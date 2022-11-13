a9e6f8bc7c7560def1f08c99e829023f39f0eaec
HebaSalem <puupiiiii@gmail.com>
2022-11-13 23:30:37 +0100

    include index.html first task

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,27 @@
  */
 
 const http = require('http');
+const fs = require('fs')
 
 //create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+
+	//Hello world task
+	// res.write('Hello World!'); // Sends a response back to the client
+	// res.end(); // Ends the response
+
+	// YOUR CODE GOES IN HERE
+	//include index.html
+	if(req.url === '/') {
+    fs.readFile('index.html' , (err , content) => {
+      if(err) throw err
+      res.writeHead(200 , {'Content-Type' : 'text/html'})
+      res.end(content , 'utf8')
+    })
+    return
+  }
+
+
 });
 
 server.listen(3000); // The server starts to listen on port 3000
