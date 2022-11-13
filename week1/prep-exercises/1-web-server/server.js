2866b32c247076f5d2b1c280b24310548f24462f
HebaSalem <43797480+HebaMak@users.noreply.github.com>
2022-11-14 00:42:41 +0200

    Update server.js

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -8,41 +8,41 @@ const fs = require('fs')
 //create a server
 let server = http.createServer(function (req, res) {
 
-	//Hello world task
-	// res.write('Hello World!'); // Sends a response back to the client
-	// res.end(); // Ends the response
+//Hello world task
+// res.write('Hello World!'); // Sends a response back to the client
+// res.end(); // Ends the response
 
-	// YOUR CODE GOES IN HERE
+// YOUR CODE GOES IN HERE
 	//include index.html
 	if(req.url === '/') {
-    fs.readFile('index.html' , (err , content) => {
-      if(err) throw err
-      res.writeHead(200 , {'Content-Type' : 'text/html'})
-      res.end(content , 'utf8')
-    })
-    return
-  }
+	    fs.readFile('index.html' , (err , content) => {
+	      if(err) throw err
+	      res.writeHead(200 , {'Content-Type' : 'text/html'})
+	      res.end(content , 'utf8')
+	    })
+	    return
+	  }
 
 	//include index.js
 	if(req.url === '/index.js') {
-    fs.readFile('index.js' , (err , content) => {
-      if(err) throw err
-      res.writeHead(200 , {'Content-Type' : 'application/javascript'})
-      res.end(content , 'utf8')
-    })
-    return
-  }
+	    fs.readFile('index.js' , (err , content) => {
+	      if(err) throw err
+	      res.writeHead(200 , {'Content-Type' : 'application/javascript'})
+	      res.end(content , 'utf8')
+	    })
+	    return
+	  }
 
 	//include style.css
 	if(req.url === '/style.css') {
-    fs.readFile('style.css' , (err , content) => {
-      if(err) throw err
-      res.writeHead(200 , {'Content-Type' : 'text/css'})
-      res.end(content , 'utf8')
-      return
-    })
-    return
-  }
+	    fs.readFile('style.css' , (err , content) => {
+	      if(err) throw err
+	      res.writeHead(200 , {'Content-Type' : 'text/css'})
+	      res.end(content , 'utf8')
+	      return
+	    })
+	    return
+	  }
 
 });
 
