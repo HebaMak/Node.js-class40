c5f11cf4adf4362ffa061dca58f2b98f953910c2
HebaSalem <puupiiiii@gmail.com>
2022-11-18 16:07:50 +0100

    add post and upadte post

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,39 @@
 const express = require('express')
+const fs = require("fs");
+
 const app = express();
- 
+app.use(express.json())
 
 // YOUR CODE GOES IN HERE
+//read blogs
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
-app.listen(3000)
+
+//create a post
+app.post('/blogs', (req, res) => {
+    // How to get the title and content from the request??
+    const {title , content} = req.body;
+    fs.writeFileSync(title, content);
+    res.end('ok')
+})
+
+//update a post
+app.put('/posts/:title', (req, res) => {
+  // How to get the title and content from the request?
+  const title = req.params.title
+  const content = req.body.content
+  
+  // What if the request does not have a title and/or content?
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.end('ok')
+  }
+  else {
+    res.end('This post does not exist!');
+  }
+})
+
+
+
+app.listen(3000 , ()=> console.log('server is running at 3000'))
