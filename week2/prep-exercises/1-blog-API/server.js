6991bbe2c01d4508b24f0bf8f20ed567e1340171
HebaSalem <puupiiiii@gmail.com>
2022-11-18 16:59:20 +0100

    get title of all files

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -17,19 +17,31 @@ app.get('/blogs/:title', (req, res) => {
   const title = req.params.title
 
   // check if post exists
-  const post = fs.readFileSync(title);
+  const post = fs.readFileSync(`./posts/${title}`);
 
-  if(fs.existsSync(title)) {
+  if(fs.existsSync(`./posts/${title}`)) {
     // send response
     res.end(post)
   }
 })
 
+//get all blogs
+app.get('/blogs', (req, res) => {
+  // how to get the file names of all files in a folder??
+  const posts = fs.readdirSync('./posts');
+  const postsTitles = posts.map((post) => {
+    return {
+      title: post,
+    };
+  });
+  res.send(postsTitles);
+});
+
 //create a post
 app.post('/blogs', (req, res) => {
     // How to get the title and content from the request??
     const {title , content} = req.body;
-    fs.writeFileSync(title, content);
+    fs.writeFileSync(`./posts/${title}`, content);
     res.end('ok')
 })
 
@@ -40,8 +52,8 @@ app.put('/posts/:title', (req, res) => {
   const content = req.body.content
   
   // What if the request does not have a title and/or content?
-  if (fs.existsSync(title)) {
-    fs.writeFileSync(title, content);
+  if (fs.existsSync(`./posts/${title}`)) {
+    fs.writeFileSync(`./posts/${title}`, content);
     res.end('ok')
   }
   else {
@@ -54,14 +66,22 @@ app.delete('/blogs/:title', (req, res) => {
   // How to get the title from the url parameters?
   const title = req.params.title
 
-  if (fs.existsSync(title)) { // Add condition here
-    fs.unlinkSync(title);
+  if (fs.existsSync(`./posts/${title}`)) { // Add condition here
+    fs.unlinkSync(`./posts/${title}`);
     res.end('ok');
   } else {
     res.end('This post does not exist!');
   }
 })
 
+app.listen(3000, () => {
 
-
-app.listen(3000 , ()=> console.log('server is running at 3000'))
+  //create folder posts if not exist
+  const postsFolder = './posts';
+  if (!fs.existsSync(postsFolder)) {
+    fs.mkdirSync(postsFolder);
+  } else {
+    console.log(`${postsFolder} is already exists`);
+  }
+  console.log('server is running');
+});
