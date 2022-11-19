cb493e1192565d7d4e274da310b553ff0700cc7f
HebaSalem <puupiiiii@gmail.com>
2022-11-19 12:56:59 +0100

    update the fetching data process

--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -9,17 +9,20 @@ app.get('/' , (req , res) => {
   res.end('hello from backend to frontend!')
 })
 
-app.post('/weather' , (req , res) => {
+app.post('/weather' , async(req , res) => {
   const city = req.body.cityName
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`)
-  .then(response => response.json())
-  .then(data => {
-    if(city) {
+  try{
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_Key}`)
+    const data = await response.json()
+    if(response.ok) {
       res.json({weatherText: `${data.name} , ${data.main.temp}°F`})
     } else {
       res.status(400).json({weatherText: "City is not found!"})
     }
-  })
+  }catch(error){
+    res.status(400).end("Error in API")
+  }
+  
 })
 
 export default app 
